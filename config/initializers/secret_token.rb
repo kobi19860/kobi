bc44f1a90bfc2d59b15a073561246e237f2a97cd
kobi <kobi19860@walla.com>
2013-07-13 14:01:01 +0300

    Created a rails application and added updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yourstoey::Application.config.secret_token = 'd1d99b74c69bfc2edca445b9c9dc8264690770f06869fde157f75c7c29c45dd1430783ef9b542a6cee92fa781c42f8fce770bbee92d5cb7306a085abb7774ee8'
